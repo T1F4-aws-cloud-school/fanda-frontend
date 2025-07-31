ca263091b36d8e1ec37313421cc865b46a0c785c
simu88 <tlan88@naver.com>
2025-07-31 10:43:27 +0900

    HomeLogined 추가(localhost/home) & 그리드 레이아웃 수정

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "../pages/Home"
+import HomeLogined from "../pages/HomeLogined"
 import Login from "../pages/Login"
 import SignUp from "../pages/SignUp"
 import PrivacyPolicy from "../pages/PrivacyPolicy"
@@ -15,6 +16,7 @@ const Router = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/home" element={<HomeLogined />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/terms/privacy" element={<PrivacyPolicy />} />
