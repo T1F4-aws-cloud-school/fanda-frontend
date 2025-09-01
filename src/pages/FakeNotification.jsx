8e73866d86b24519968e6faa50afc18104fd7778
jjeonge38 <131588594+jjeonge38@users.noreply.github.com>
2025-09-01 11:04:12 +0900

    fix/noti (#178)
    Co-authored-by: jjeonge38 <kshjm151217@namer.com>

--- src/pages/FakeNotification.jsx
+++ src/pages/FakeNotification.jsx
@@ -17,8 +17,8 @@ const FakeNotification = ({ show, onHide }) => {
       <div className="notification-content">
         <div className="notification-icon"></div>
         <div className="notification-text">
-          <div className="notification-title">고객님이 좋아할 상품했던,</div>
-          <div className="notification-subtitle">깔끔 바베큐 크기가 더 커졌어요!</div>
+          <div className="notification-title">고객님이 좋아할만한 상품!,</div>
+          <div className="notification-subtitle">신선 연어 크기가 더 커지고, 품질이 개선되었어요!</div>
         </div>
       </div>
     </div>
